3027b626cc1e9ba24ff26558e988f45c521ef428
kimiaki704 <kimiaki704@gmail.com>
2020-05-05 07:05:22 +0900

    🍹 Add PresenterResolver.swift

---
+++ FakeInstagram/Shared/Resolver/PresenterResolver.swift
@@ -0,0 +1,12 @@
+//
+//  PresenterResolver.swift
+//  FakeInstagram
+//
+//  Created by 鈴木 公章 on 2020/05/05.
+//
+
+extension Resolver {
+    func resolveListPresenter() -> ListPresenter {
+        ListPresenter()
+    }
+}
