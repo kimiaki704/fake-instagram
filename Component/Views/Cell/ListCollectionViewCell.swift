3fb44c2331e57beeed557eb769cea9a87e63ccae
kimiaki704 <kimiaki704@gmail.com>
2020-05-05 06:19:00 +0900

    🐤 Update ListCollectionViewCell

--- Component/Views/Cell/ListCollectionViewCell.swift
+++ Component/Views/Cell/ListCollectionViewCell.swift
@@ -5,6 +5,7 @@
 //  Created by 鈴木 公章 on 2020/05/05.
 //
 
+import Model
 import UIKit
 
 public class ListCollectionViewCell: UICollectionViewCell, NibLoadableView, ReusableView {
@@ -15,9 +16,23 @@ public class ListCollectionViewCell: UICollectionViewCell, NibLoadableView, Reus
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var likeButton: UIButton!
     @IBOutlet private var commentButton: UIButton!
+    public var likeButtonTappedAction: ((UICollectionViewCell) -> Void)?
+    public var commentButtonTappedAction: ((UICollectionViewCell) -> Void)?
+
+    public func setup(_ post: Post) {
+        userImageView.loadImage(post.user.imageURL, processors: [.circle])
+        userNameLabel.text = post.user.name
+        createdAtLabel.text = post.createdAt
+        textLabel.text = post.text
+        likeButton.setTitle(" Likes \(post.likeCount)", for: .normal)
+        if post.isLike { likeButton.tintColor = .red }
+        if !post.isLike { likeButton.tintColor = .darkText }
+        guard let imageURL = post.imageURL else { return }
+        imageView.loadImage(imageURL)
+    }
 }
 
 extension ListCollectionViewCell {
-    @IBAction private func likeButtonTapped(_ sender: UIButton) {}
-    @IBAction private func commentButtonTapped(_ sender: UIButton) {}
+    @IBAction private func likeButtonTapped(_ sender: UIButton) { likeButtonTappedAction?(self) }
+    @IBAction private func commentButtonTapped(_ sender: UIButton) { commentButtonTappedAction?(self) }
 }
--- Model/Entity/User.swift
+++ Model/Entity/User.swift
@@ -7,18 +7,18 @@
 
 public struct User {
     public var id: String
-    public var fullName: String
+    public var name: String
     public var email: String
     public var imageURL: String
     public var headerImageURL: String
 
     init(id: String,
-         fullName: String,
+         name: String,
          email: String,
          imageURL: String,
          headerImageURL: String) {
         self.id = id
-        self.fullName = fullName
+        self.name = name
         self.email = email
         self.imageURL = imageURL
         self.headerImageURL = headerImageURL
@@ -29,7 +29,7 @@ public struct User {
         let image2 = "https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/c0.93.750.750a/s640x640/70235785_541547256606285_1892577763965236759_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=fCkIvOzHqWkAX_PoZGo&oh=6dfe1e1b4210e550bc3cc33102e6ce06&oe=5EC65736"
         let headerImage1 = "https://pbs.twimg.com/media/DMWZfvuUQAATQPL.jpg"
         let headerImage2 = "https://images-na.ssl-images-amazon.com/images/I/51U414%2BaZDL._AC_SX466_.jpg"
-        return [User(id: "f1", fullName: "Steave Jobs", email: "steave@info.com", imageURL: image1, headerImageURL: headerImage1),
-                User(id: "f2", fullName: "Mark", email: "mark@info.com", imageURL: image2, headerImageURL: headerImage2)]
+        return [User(id: "f1", name: "Steave Jobs", email: "steave@info.com", imageURL: image1, headerImageURL: headerImage1),
+                User(id: "f2", name: "Mark", email: "mark@info.com", imageURL: image2, headerImageURL: headerImage2)]
     }
 }
