91920b2026b2d167d579a4519bfb610a060d2f1e
kimiaki704 <kimiaki704@gmail.com>
2020-05-05 04:24:38 +0900

    🍹 Add Model framework

---
+++ Model/Model.h
@@ -0,0 +1,18 @@
+//
+//  Model.h
+//  Model
+//
+//  Created by 鈴木 公章 on 2020/05/05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Model.
+FOUNDATION_EXPORT double ModelVersionNumber;
+
+//! Project version string for Model.
+FOUNDATION_EXPORT const unsigned char ModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Model/PublicHeader.h>
+
+
