fd8a9d279fcbc4913083426a9448cfaf1125946c
kimiaki704 <kimiaki704@gmail.com>
2020-05-05 01:38:38 +0900

    🍹 Add ListContract.swift

---
+++ FakeInstagram/Pages/List/Contract/ListContract.swift
@@ -0,0 +1,28 @@
+//
+//  ListContract.swift
+//  FakeInstagram
+//
+//  Created by 鈴木 公章 on 2020/05/04.
+//
+
+protocol ListView: class {
+    var presenter: ListPresentation! { get set }
+}
+
+protocol ListPresentation: class {
+    var view: ListView? { get set }
+    var interactor: ListUseCase! { get set }
+    var router: ListWireframe! { get set }
+
+    func viewDidLoad()
+}
+
+protocol ListUseCase: class {
+    var output: ListInteractorOutput! { get set }
+}
+
+protocol ListInteractorOutput: class {}
+
+protocol ListWireframe: class {
+    static var appResolver: Resolver { get }
+}
