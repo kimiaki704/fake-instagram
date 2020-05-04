e764c88372da72042a1778e5064e329ee597b6b0
kimiaki704 <kimiaki704@gmail.com>
2020-05-05 02:23:44 +0900

    🐤 Update ListViewController

--- FakeInstagram/Pages/List/View/ListViewController.swift
+++ FakeInstagram/Pages/List/View/ListViewController.swift
@@ -8,4 +8,42 @@
 import Component
 import UIKit
 
-final class ListViewController: UIViewController, Instantiatable {}
+final class ListViewController: UIViewController, Instantiatable {
+    @IBOutlet private var collectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<ListCollectionViewSection, AnyHashable>!
+    var presenter: ListPresentation!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        presenter.viewDidLoad()
+    }
+
+    private func setupUI() {
+        title = "List"
+    }
+}
+
+extension ListViewController {
+    private func setupCollectionView() {
+        collectionView.compositionalLayout(itemWidthDimension: .fractionalWidth(1.0),
+                                           itemHeightDimension: .estimated(406))
+        collectionView.register(ListCollectionViewCell.self)
+    }
+
+    func performSnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<ListCollectionViewSection, AnyHashable>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems([])
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<ListCollectionViewSection, AnyHashable>(collectionView: collectionView) { collectionView, indexPath, item -> UICollectionViewCell? in
+            let cell: ListCollectionViewCell = collectionView.dequeueReusableCell(forIndexPath: indexPath)
+            return cell
+        }
+    }
+}
+
+extension ListViewController: ListView {}
