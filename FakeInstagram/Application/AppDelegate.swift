cfc788e2fd85da6d9400e0856c74aaa685416588
kimiaki704 <kimiaki704@gmail.com>
2020-05-04 19:35:56 +0900

    🐤 Update setting

---
+++ FakeInstagram/Application/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  FakeInstagram
+//
+//  Created by Suzuki Kimiaki on 2020/05/01.
+//  Copyright © 2020 Suzuki Kimiaki. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
