893b8b7609f4b769beb6ddab0dc20a5934faa194
kimiaki704 <kimiaki704@gmail.com>
2020-05-05 23:30:05 +0900

    🍹 Add PostViewController.swift

---
+++ FakeInstagram/Pages/Post/View/PostViewController.swift
@@ -0,0 +1,11 @@
+//
+//  PostViewController.swift
+//  FakeInstagram
+//
+//  Created by 鈴木 公章 on 2020/05/05.
+//
+
+import UIKit
+import Component
+
+final class PostViewController: UIViewController, Instantiatable {}
